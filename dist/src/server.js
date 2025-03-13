6c0f338f083335bf5fb918072885c4917cb02b48
riyazriya010 <riyur017@gmail.com.com>
2025-03-13 16:33:51 +0530

    cors setting applied

--- dist/src/server.js
+++ dist/src/server.js
@@ -6,12 +6,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const restaurant_1 = __importDefault(require("./routes/restaurant"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const PORT = 5005;
 const app = (0, express_1.default)();
 app.use(express_1.default.json()); // ✅ This allows Express to parse JSON requests
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use("/user-service", restaurant_1.default);
+app.use((0, cors_1.default)({
+    origin: [
+        "http://127.0.0.1:5173",
+        "http://localhost:5173",
+        "http://localhost:5005",
+    ],
+    methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
+    credentials: true, // ✅ Allowed outside 'origin'
+}));
 app.listen(PORT, (error) => {
     if (error)
         throw error;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from 'express'
 import dotenv from 'dotenv'
 import restaurantRoutes from './routes/restaurant'
+import cors from 'cors'
 dotenv.config()
 
 const PORT = 5005
@@ -9,6 +10,20 @@ const app = express()
 app.use(express.json()); // ✅ This allows Express to parse JSON requests
 app.use(express.urlencoded({ extended: true })); 
 app.use("/user-service", restaurantRoutes)
+
+app.use(
+    cors({
+      origin: [
+        "http://127.0.0.1:5173",
+        "http://localhost:5173",
+        "http://localhost:5005",
+      ],
+      methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
+      allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
+      credentials: true, // ✅ Allowed outside 'origin'
+    })
+  );
+  
 
 app.listen(PORT, (error: any) => {
     if(error) throw error
